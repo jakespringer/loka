c040f204cd7f6eb3a938ceabe159acf3cf3c76e7
Jake <me@jakespringer.com>
2017-01-14 20:59:49 -0800

    Fixed copy-replace problem + made code more concise

--- Server/src/Loka.hs
+++ Server/src/Loka.hs
@@ -57,13 +57,13 @@ collapseGameState state = foldl collapseSingleState [] $ map (\(_, move) -> move
 -- | Returns a list of all possible moves that can be made on the board for a
 -- given color.
 allPossibleMoves :: CollapsedGameState -> PieceColor -> [GameMove]
-allPossibleMoves state col = concat $ map (allPossiblePieceTerrainMoves state) $
-  filter (matchesColor col) state
+allPossibleMoves state pieceColor = concat $ map (allPossiblePieceTerrainMoves state) $
+  filter (matchesColor pieceColor) state
   where
     allPossiblePieceTerrainMoves state (Left x) = allPossiblePieceMoves state x
     allPossiblePieceTerrainMoves _ _ = []
 
-    matchesColor col (Left (Piece _ _ _ check)) = check == col
+    matchesColor pieceColor (Left (Piece _ _ _ check)) = check == pieceColor
     matchesColor _ _ = False
 
 ------------------------------------------------------------------------------
@@ -79,54 +79,36 @@ allPossiblePieceMoves state piece = filter (\x -> checkMove x state) $
     bishopMovePiece piece dir dist = (MovePiece (BishopMove dir dist)) piece
     knightMovePiece piece dir = (MovePiece (KnightMove dir)) piece
 
-    constructMoves state (Piece x y Queen col) =
-         map (queenMovePiece (Piece x y Queen col) DirectionUp) [1..12]
-      ++ map (queenMovePiece (Piece x y Queen col) DirectionUpRight) [1..12]
-      ++ map (queenMovePiece (Piece x y Queen col) DirectionRight) [1..12]
-      ++ map (queenMovePiece (Piece x y Queen col) DirectionDownRight) [1..12]
-      ++ map (queenMovePiece (Piece x y Queen col) DirectionDown) [1..12]
-      ++ map (queenMovePiece (Piece x y Queen col) DirectionDownLeft) [1..12]
-      ++ map (queenMovePiece (Piece x y Queen col) DirectionLeft) [1..12]
-      ++ map (queenMovePiece (Piece x y Queen col) DirectionUpLeft) [1..12]
+    constructMoves state (Piece x y Queen pieceColor) =
+      concat $ map (\dir -> map (queenMovePiece (Piece x y Queen pieceColor) dir) [1..12])
+        [ DirectionUp, DirectionUpRight, DirectionRight, DirectionDownRight
+        , DirectionDown, DirectionDownLeft, DirectionLeft, DirectionUpLeft ]
 
-    constructMoves state (Piece x y King col) =
-      [ kingMovePiece (Piece x y King col) DirectionUp
-      , kingMovePiece (Piece x y King col) DirectionUpRight
-      , kingMovePiece (Piece x y King col) DirectionRight
-      , kingMovePiece (Piece x y King col) DirectionDownRight
-      , kingMovePiece (Piece x y King col) DirectionDown
-      , kingMovePiece (Piece x y King col) DirectionDownLeft
-      , kingMovePiece (Piece x y King col) DirectionLeft
-      , kingMovePiece (Piece x y King col) DirectionUpLeft]
+    constructMoves state (Piece x y King pieceColor) =
+      map (\dir -> kingMovePiece (Piece x y King pieceColor) dir)
+       [ DirectionUp, DirectionUpRight, DirectionRight, DirectionDownRight
+       , DirectionDown, DirectionDownLeft, DirectionLeft, DirectionUpLeft ]
 
-    constructMoves state (Piece x y Rook col) =
-         map (rookMovePiece (Piece x y Rook col) DirectionUp) [1..12]
-      ++ map (rookMovePiece (Piece x y Rook col) DirectionRight) [1..12]
-      ++ map (rookMovePiece (Piece x y Rook col) DirectionDown) [1..12]
-      ++ map (rookMovePiece (Piece x y Rook col) DirectionLeft) [1..12]
+    constructMoves state (Piece x y Rook pieceColor) =
+      concat $ map (\dir -> map (rookMovePiece (Piece x y Rook pieceColor) dir) [1..12])
+        [ DirectionUp, DirectionRight, DirectionDown, DirectionLeft ]
 
-    constructMoves state (Piece x y Bishop col) =
-         map (bishopMovePiece (Piece x y Bishop col) DirectionUpRight) [1..12]
-      ++ map (bishopMovePiece (Piece x y Bishop col) DirectionDownRight) [1..12]
-      ++ map (bishopMovePiece (Piece x y Bishop col) DirectionDownLeft) [1..12]
-      ++ map (bishopMovePiece (Piece x y Bishop col) DirectionUpLeft) [1..12]
+    constructMoves state (Piece x y Bishop pieceColor) =
+      concat $ map (\dir -> map (bishopMovePiece (Piece x y Bishop pieceColor) dir) [1..12])
+        [ DirectionUpRight, DirectionDownRight, DirectionDownLeft, DirectionUpLeft ]
 
-    constructMoves state (Piece x y Knight col) =
-      [ knightMovePiece (Piece x y Knight col) KnightDirectionLeftUp
-      , knightMovePiece (Piece x y Knight col) KnightDirectionLeftDown
-      , knightMovePiece (Piece x y Knight col) KnightDirectionRightUp
-      , knightMovePiece (Piece x y Knight col) KnightDirectionRightDown
-      , knightMovePiece (Piece x y Knight col) KnightDirectionUpRight
-      , knightMovePiece (Piece x y Knight col) KnightDirectionUpLeft
-      , knightMovePiece (Piece x y Knight col) KnightDirectionDownRight
-      , knightMovePiece (Piece x y Knight col) KnightDirectionDownLeft]
+    constructMoves state (Piece x y Knight pieceColor) =
+      map (\dir -> knightMovePiece (Piece x y Knight pieceColor) dir)
+        [ KnightDirectionLeftUp, KnightDirectionLeftDown
+        , KnightDirectionRightUp, KnightDirectionRightDown
+        , KnightDirectionUpRight, KnightDirectionUpLeft
+        , KnightDirectionDownRight, KnightDirectionDownLeft]
 
-    constructMoves state (Piece x y Pawn col) =
-      [ MovePiece PawnMove (Piece x y Pawn col) ]
-
+    constructMoves state (Piece x y Pawn pieceColor) =
+      [ MovePiece PawnMove (Piece x y Pawn pieceColor) ]
 
 ------------------------------------------------------------------------------
--- | UtilipieceType function that converts (x, y) coordinates to new coordinates based
+-- | Utility function that converts (x, y) coordinates to new coordinates based
 -- on move made.
 pieceMoveCoordinates :: PieceDirection -> Integer -> (Integer, Integer)
   -> (Integer, Integer)
@@ -135,14 +117,14 @@ pieceMoveCoordinates dir dist coord = (dist
   `vecAdd` coord
 
 ------------------------------------------------------------------------------
--- | UtilipieceType function that converts (x, y) coordinates to new coordinates based
+-- | Utility function that converts (x, y) coordinates to new coordinates based
 -- on knight move made.
 knightMoveCoordinates :: KnightDirection -> (Integer, Integer) -> (Integer, Integer)
 knightMoveCoordinates kDir coord = coord
   `vecAdd` knightDirectionCoordinates kDir
 
 ------------------------------------------------------------------------------
--- | UtilipieceType function that converts a PieceDirection to its corresponding
+-- | Utility function that converts a PieceDirection to its corresponding
 -- coordinate change.
 pieceDirectionCoordinates :: PieceDirection -> (Integer, Integer)
 pieceDirectionCoordinates DirectionRight = (1, 0)
@@ -155,7 +137,7 @@ pieceDirectionCoordinates DirectionDown = (0, -1)
 pieceDirectionCoordinates DirectionDownRight = (1, -1)
 
 ------------------------------------------------------------------------------
--- | UtilipieceType function that converts a KnightDirection to its corresponding
+-- | Utility function that converts a KnightDirection to its corresponding
 -- coordinate change.
 knightDirectionCoordinates :: KnightDirection -> (Integer, Integer)
 knightDirectionCoordinates KnightDirectionUpRight = (1, 2)
@@ -168,7 +150,7 @@ knightDirectionCoordinates KnightDirectionLeftDown = (-2, -1)
 knightDirectionCoordinates KnightDirectionLeftUp = (-2, 1)
 
 ------------------------------------------------------------------------------
--- | UtilipieceType function that converts a PieceColor to its corresponding
+-- | Utility function that converts a PieceColor to its corresponding
 -- pawn direction.
 pawnColorDirection :: PieceColor -> PieceDirection
 pawnColorDirection Red = DirectionUp
@@ -177,23 +159,23 @@ pawnColorDirection Green = DirectionRight
 pawnColorDirection Yellow = DirectionLeft
 
 ------------------------------------------------------------------------------
--- | UtilipieceType function that checks if a coordinate is in the board bounds.
+-- | Utility function that checks if a coordinate is in the board bounds.
 inBoardBounds :: Integer -> Integer -> Bool
 inBoardBounds x y = 0 <= x && x < 12 && 0 <= y && y < 12
   && ((4 <= x && x < 9) || (4 <= y && y < 9))
 
 ------------------------------------------------------------------------------
--- | UtilipieceType function that adds two vectors.
+-- | Utility function that adds two vectors.
 vecAdd :: Num a => (a, a) -> (a, a) -> (a, a)
 vecAdd (x, y) (z, w) = (x+z, y+w)
 
 ------------------------------------------------------------------------------
--- | UtilipieceType function that multiplies a scalar and a vector.
+-- | Utility function that multiplies a scalar and a vector.
 vecTimes :: Num a => a -> (a, a) -> (a, a)
 vecTimes scalar (x, y) = (x*scalar, y*scalar)
 
 ------------------------------------------------------------------------------
--- | UtilipieceType function that checks if you can enter certain terrain.
+-- | Utility function that checks if you can enter certain terrain.
 canEnterTerrain :: TerrainType -> PieceType -> PieceDirection -> Bool
 canEnterTerrain Eyrie Knight _ = True
 canEnterTerrain Eyrie _ _ = False
@@ -218,15 +200,29 @@ canExitTerrain :: TerrainType -> PieceType -> PieceDirection -> Bool
 canExitTerrain Swamp _ _ = False
 canExitTerrain _ _ _ = True
 
-
 ------------------------------------------------------------------------------
 -- | A number of utilipieceType functions that are used in checkMove
 
 -- Helper functions to check if a given piece/terrain has a given x,y
 xyPieceEquals xCheck yCheck (Left (Piece x y _ _)) = x == xCheck && y == yCheck
 xyPieceEquals _ _ (Right _) = False
+
 xyTerrainEquals xCheck yCheck (Right (Terrain x y _)) = x == xCheck && y == yCheck
 xyTerrainEquals _ _ (Left _) = False
+
+-- Checks if a direction is cardinal
+isCardinal :: PieceDirection -> Bool
+isCardinal DirectionRight = True
+isCardinal DirectionUpRight = False
+isCardinal DirectionUp = True
+isCardinal DirectionUpLeft = False
+isCardinal DirectionLeft = True
+isCardinal DirectionDownLeft = False
+isCardinal DirectionDown = True
+isCardinal DirectionDownRight = False
+
+-- Checks if a direction is diagonal
+isDiagonal = not . isCardinal
 
 -- Returns a square of distance dist away from x,y in direction dir
 lastSquare dir x y dist = (dist `vecTimes` pieceDirectionCoordinates dir)
@@ -252,7 +248,7 @@ canExitSquare static pType dir x y = case find (xyTerrainEquals x y) static of
   Nothing -> True
   _ -> True
 
--- Checks ifi a given piece can walk along every square in the given direction
+-- Checks if a given piece can walk along every square in the given direction
 canTraverseBoard :: Piece -> PieceDirection -> Integer -> CollapsedGameState -> Bool
 canTraverseBoard (Piece x y pieceType _) dir dist static =
   (all (uncurry (canEnterSquare static pieceType dir)) $ straightLine dir dist x y)
@@ -262,34 +258,24 @@ canTraverseBoard (Piece x y pieceType _) dir dist static =
 -- | Checks if a given move is valid.
 checkMove :: GameMove -> CollapsedGameState -> Bool
 checkMove Noop _ = True
-checkMove (PlacePiece (Piece x y pieceType col)) state = all (not . xyPieceEquals x y) state
+checkMove (PlacePiece (Piece x y pieceType pieceColor)) state = all (not . xyPieceEquals x y) state
 checkMove (PlaceTerrain (Terrain x y pieceType)) state = all (not . xyTerrainEquals x y) state
-checkMove (MovePiece (KingMove dir) (Piece x y King col)) state =
-  canTraverseBoard (Piece x y King col) dir 1 state
-checkMove (MovePiece (QueenMove dir dist) (Piece x y Queen col)) state =
-  canTraverseBoard (Piece x y Queen col) dir dist state
-checkMove (MovePiece (RookMove dir dist) (Piece x y Rook col)) state =
-  checkRookDirection dir && checkCanTraverseBoard
+checkMove (MovePiece (KingMove dir) (Piece x y King pieceColor)) state =
+  canTraverseBoard (Piece x y King pieceColor) dir 1 state
+checkMove (MovePiece (QueenMove dir dist) (Piece x y Queen pieceColor)) state =
+  canTraverseBoard (Piece x y Queen pieceColor) dir dist state
+checkMove (MovePiece (RookMove dir dist) (Piece x y Rook pieceColor)) state =
+  isCardinal dir && checkCanTraverseBoard
     where
-      checkCanTraverseBoard = canTraverseBoard (Piece x y Rook col) dir dist state
-      checkRookDirection DirectionRight = True
-      checkRookDirection DirectionLeft = True
-      checkRookDirection DirectionUp = True
-      checkRookDirection DirectionDown = True
-      checkRookDirection _ = False
-checkMove (MovePiece (BishopMove dir dist) (Piece x y Bishop col)) state =
-  checkBishopDirection dir && checkCanTraverseBoard
+      checkCanTraverseBoard = canTraverseBoard (Piece x y Rook pieceColor) dir dist state
+checkMove (MovePiece (BishopMove dir dist) (Piece x y Bishop pieceColor)) state =
+  isDiagonal dir && checkCanTraverseBoard
     where
-      checkCanTraverseBoard = canTraverseBoard (Piece x y Bishop col) dir dist state
-      checkBishopDirection DirectionUpRight = True
-      checkBishopDirection DirectionUpLeft = True
-      checkBishopDirection DirectionDownRight = True
-      checkBishopDirection DirectionDownLeft = True
-      checkBishopDirection _ = False
-checkMove (MovePiece (KnightMove kDir) (Piece x y Knight col)) state =
+      checkCanTraverseBoard = canTraverseBoard (Piece x y Bishop pieceColor) dir dist state
+checkMove (MovePiece (KnightMove kDir) (Piece x y Knight pieceColor)) state =
   (uncurry inBoardBounds) ((x, y) `vecAdd` (knightDirectionCoordinates kDir) )
     && (uncurry $ canEnterSquare state Knight DirectionUp) ((x, y)
       `vecAdd` (knightDirectionCoordinates kDir) )
-checkMove (MovePiece PawnMove (Piece x y Pawn col)) state =
-  canTraverseBoard (Piece x y Pawn col) (pawnColorDirection col) 1 state
+checkMove (MovePiece PawnMove (Piece x y Pawn pieceColor)) state =
+  canTraverseBoard (Piece x y Pawn pieceColor) (pawnColorDirection pieceColor) 1 state
 checkMove _ _ = False
